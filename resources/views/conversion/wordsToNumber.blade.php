88bc2ce1dd5aabf59b66f42e8c83e06c372c342f
NelsonRyan-06 <nelsonarabit00@gmail.com>
2023-06-29 09:50:05 +0800

    add comments

--- resources/views/conversion/wordsToNumber.blade.php
+++ resources/views/conversion/wordsToNumber.blade.php
@@ -5,9 +5,14 @@
 @section('content')
 <div class="text-white max-w-xl">
   <h1 class="text-4xl mb-5 -mt-32">Convert your words to number</h1>
+
+ {{-- WORDS TO NUMBER FORM --}}
+
   <form method="POST" action="{{  route('convert.convertWords') }}" class="flex flex-col">
     @csrf
     <label for="words" class="mb-3 text-lg">Enter words:</label>
+
+    {{-- INPUT --}}
     @empty($result->input)
     <input type="text" id="words" name="input" class="max-w-xs py-1.5 px-5 rounded mb-10 text-black" placeholder="Enter words" required>
     @else
@@ -15,16 +20,22 @@
     @endempty
     <button type="submit" class="w-fit bg-red-800 rounded-md px-6 py-1.5">Convert to number</button>
   </form>
+
+    {{-- OUTPUT --}}
+
     @empty($result->output)
       <input name="output" class="text-gray-800 bg-gray-300 font-semibold text-lg py-1 px-5 rounded mt-5" type="text" disabled/>
     @else
       <input name="output" class="text-gray-800 bg-gray-300 font-semibold text-lg py-1 px-5 rounded mt-5" type="text" value="{{ $result->output}}" disabled/>
     @endempty
 
+    {{-- PHP TO USD CONVERSION --}}
+
     @empty($result->converted) 
     @else
       <input name="convertedOutput" class="block w-full text-gray-800 bg-gray-300 font-semibold text-lg py-1 px-5 rounded mt-5" type="text" value="If we convert P{{ number_format($result->output, 2)  }} to USD, the amount is ${{ number_format($result->converted->conversion_result, 2)}}"/>
     @endempty
+    
     <button id="toNumberConversion" class="underline text-sm mt-2 hover:text-red-700 block">Want to convert number to words?</button>
 </div>
 
